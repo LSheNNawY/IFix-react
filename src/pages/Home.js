96dc0510396843b4bbad244bebdae17640e95249
IslamAhmed0 <ea43533@gmail.com>
2021-06-09 19:38:51 +0200

    edit home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,7 +15,7 @@ const Home = () => {
     useEffect(() => {
         ajaxGetProfessions(3).then((data) => {
             setProfessions(data);
-            // console.log(data);
+            console.log(data);
         })
     }, [])
     return (
@@ -23,16 +23,37 @@ const Home = () => {
             <NavbarComponent/>
             <HeaderComponent/>
 
-            <section className="category">
+            <section className="category " style={{padding:"120px 0"}}>
                 <div className="container">
-                    <div className="row">
-                        {
-                            professions.map(profession => <ProfessionComponent profession={profession} key={profession._id}/>)
-                        }
+                    <div className="row ">
+                        <div className="images col-12">
+                            <div className="row justify-content-center align-items-center align-baseline">
+
+                                {
+                                    professions.map(profession => <ProfessionComponent profession={profession} key={profession._id}/>)
+                                }
+                            </div>
+
+                        </div>
+
                     </div>
                 </div>
             </section>
+            {/*<section>*/}
+            {/*    <div className="container">*/}
+            {/*        <div className="row ">*/}
+            {/*            <div className="col-12">*/}
+            {/*                <h4>How Fixawy Works?</h4>*/}
+            {/*                <div className="col-md-4">*/}
+            {/*                    <p className="lead">Place your*/}
+            {/*                        OrderWith our clear and flexible booking system you just need to choose the service, pick a suitable appointment and confirm your order to find one of our highly qualified repairmen at your doorstep.*/}
+            {/*                        1</p>*/}
+            {/*                </div>*/}
+            {/*            </div>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
 
+            {/*</section>*/}
             <FooterComponent/>
         </>
     );
