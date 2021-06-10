32409946bdfe9090dfdf0aeff9f6c420e99d762d
LSheNNawY <devshennawy@gmail.com>
2021-06-10 19:03:12 +0200

    fix phone regex

--- src/components/front/ProfessionComponent.js
+++ src/components/front/ProfessionComponent.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import '../../styles/Header.css';
+
 const ProfessionComponent = ({profession}) => {
     return (
         <>
-
             <div className="col-4 text-center">
                 <img className="img" src={`http://localhost:5000/uploads/${profession.img}`}/>
                 <p className="title">{profession.title}</p>
-
             </div>
-
         </>
     );
 }
