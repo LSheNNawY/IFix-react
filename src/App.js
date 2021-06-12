307a480a006032cac2ccf396583b0a7341511390
LSheNNawY <devshennawy@gmail.com>
2021-06-12 07:46:56 +0200

    adding dashboard

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import {Switch, Route, Redirect} from 'react-router-dom'
+import {Route, Switch, Redirect} from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
+
+import AdminLayout from "./layouts/Admin";
+
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/dashboard/css/animate.min.css";
+import "./assets/dashboard/css/light-bootstrap-dashboard-react.css";
+import "./assets/dashboard/css/demo.css";
+
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 function App() {
     return (
@@ -10,8 +19,8 @@ function App() {
             <Switch>
                 <Route path='/' component={Home} exact/>
                 <Route path='/login' component={Login}/>
-                <Redirect from='/login/' to="/" />
                 <Route path='/register' component={Register}/>
+                <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
             </Switch>
         </div>
     );
---
+++ src/routes/dashboard/routes.js
@@ -0,0 +1,53 @@
+import Dashboard from "../../pages/dashboard/Dashboard";
+import UserProfile from "../../pages/dashboard/UserProfile";
+import TableList from "../../pages/dashboard/TableList";
+import Typography from "../../pages/dashboard/Typography";
+import Maps from "../../pages/dashboard/Maps";
+import Notifications from "../../pages/dashboard/Notifications";
+
+const dashboardRoutes = [
+    {
+        path: "/dashboard",
+        name: "Dashboard",
+        icon: "fa fa-chart-pie",
+        component: Dashboard,
+        layout: "/admin",
+    },
+    {
+        path: "/user",
+        name: "User Profile",
+        icon: "fa fa-user-circle",
+        component: UserProfile,
+        layout: "/admin",
+    },
+    {
+        path: "/table",
+        name: "Table List",
+        icon: "fa fa-clipboard",
+        component: TableList,
+        layout: "/admin",
+    },
+    {
+        path: "/typography",
+        name: "Typography",
+        icon: "fa fa-scroll",
+        component: Typography,
+        layout: "/admin",
+    },
+    {
+        path: "/maps",
+        name: "Maps",
+        icon: "fa fa-map-marker-alt",
+        component: Maps,
+        layout: "/admin",
+    },
+    {
+        path: "/notifications",
+        name: "Notifications",
+        icon: "fa fa-bullhorn",
+        component: Notifications,
+        layout: "/admin",
+    },
+];
+
+export default dashboardRoutes;
