d27a1dfda08f187a456e74ea5482fdc9050ee7d7
sara-mohamed12 <78587095+sara-mohamed12@users.noreply.github.com>
2021-06-14 02:57:15 +0200

    edit in view jobs

--- src/pages/dashboard/JobTable.js
+++ src/pages/dashboard/JobTable.js
@@ -1,29 +1,47 @@
 import React, { useState, useEffect } from "react";
-
+import axios from "axios";
 // react-bootstrap components
 import { 
-    Card, 
-    Table, 
-    Container, 
-    Row, 
-    Col } from "react-bootstrap";
+  Card, 
+  Table, 
+  Container, 
+  Row, 
+  Col, 
+  Button } from "react-bootstrap";
 
-const ajaxJobs = async () => {
-  const data = await (
-    await fetch(`${process.env.REACT_APP_API_URL}/jobs`)
-  ).json();
-  return data;
-};
+// const ajaxJobs = async () => {
+//   const data = await (
+//     await fetch(`${process.env.REACT_APP_API_URL}/jobs`)
+//   ).json();
+//   return data;
+// };
 
 function Jobs() {
-  const [jobs, setJobs] = useState([]);
+  /* const [jobs, setJobs] = useState([]);
 
   useEffect(() => {
     ajaxJobs().then((data) => {
-      setJobs(data);
-      console.log(data);
+      
+        setJobs(data);
+        console.log("data jobs: ",data);
+      
+      
+      
+    });
+  }, []); */
+
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    axios.get(process.env.REACT_APP_API_URL + "/jobs").then((res) => {
+      setJobs(res.data);
+      console.log(res.data);
     });
   }, []);
+
+ /*  const deleteUser = (id) => {
+    setJobs(jobs.filter((job) => job.id !== id))
+  } */
 
   return (
     <>
@@ -41,30 +59,154 @@ function Jobs() {
                 <Table className="table-hover table-striped">
                   <thead>
                     <tr>
-                      <th className="border-0">Client</th>
-                      <th className="border-0">Employee</th>
-                      <th className="border-0">Profession</th>
-                      <th className="border-0">Service</th>
-                      <th className="border-0">Price</th>
-                      <th className="border-0">Action</th>
+                      <th className="border-0"> ID </th>
+                      <th className="border-0"> Employee </th>
+                      <th className="border-0"> Client </th>
+                      <th className="border-0"> Profession </th>
+                      <th className="border-0"> Services </th>
+                      <th className="border-0"> Warranty </th>
+                      <th className="border-0"> Payment Method </th>
+                      {/* <th className="border-0"> Description </th> */}
+                      <th className="border-0"> Price </th>
+                      <th className="border-0"> Actions </th>
                     </tr>
                   </thead>
                   <tbody>
-                    {jobs.map((job) => (
+                    {jobs.length > 0 ? (
+                      jobs.map((job, index) => {
+                        return (
+                          <tr key={index}>
+                            <td> {index + 1} </td>
+                            <td>
+                              <table table-hover table-striped>
+                                <thead>
+                                  <tr>
+                                    <th className="border-0"> Name </th>
+                                    <th className="border-0"> Phone </th>
+                                  </tr>
+                                </thead>
+                                <tbody>
+                                  {job.client.length > 0 ? (
+                                    job.client.map((client, i) => {
+                                      return (
+                                        <tr>
+                                          <td>
+                                            {job.client[i].firstName +
+                                              job.client[i].lastName}
+                                          </td>
+                                          <td>{job.client[i].phone}</td>
+                                        </tr>
+                                      );
+                                    })
+                                  ) : (
+                                    <></>
+                                  )}
+                                </tbody>
+                              </table>
+                            </td>
+                            <td>
+                              <table table-hover table-striped>
+                                <thead>
+                                  <tr>
+                                    <th className="border-0"> Name </th>
+
+                                    <th className="border-0"> Phone </th>
+                                  </tr>
+                                </thead>
+                                <tbody>
+                                  {job.employee.length > 0 ? (
+                                    job.employee.map(
+                                      (employee, i) => {
+                                        return (
+                                          <tr>
+                                            <td>
+                                              {job.employee[i].firstName}
+                                            </td>
+
+                                            <td>
+                                              {job.employee[i].phone}
+                                            </td>
+                                          </tr>
+                                        );
+                                      }
+                                    )
+                                  ) : (
+                                    <></>
+                                  )}
+                                </tbody>
+                              </table>
+                            </td>
+
+                            <td>
+                              <table table-hover table-striped>
+                                <thead>
+                                  <tr>
+                                    <th className="border-0"> Title </th>
+                                  </tr>
+                                </thead>
+                                <tbody>
+                                  {job.profession.length > 0 ? (
+                                    job.profession.map((profession, i) => {
+                                      return (
+                                        <tr>
+                                          <td>{job.profession[i].title}</td>
+                                        </tr>
+                                      );
+                                    })
+                                  ) : (
+                                    <></>
+                                  )}
+                                </tbody>
+                              </table>
+                            </td>
+
+                            <td>
+                              <table table-hover table-striped>
+                                <thead>
+                                  <tr>
+                                    <th className="border-0"> Title </th>
+
+                                    <th className="border-0"> Price </th>
+                                  </tr>
+                                </thead>
+                                <tbody>
+                                  {job.profession.services.length > 0 ? (
+                                    job.profession.services.map(
+                                      (service, i) => {
+                                        return (
+                                          <tr>
+                                            <td>
+                                              {job.profession.services[i].title}
+                                            </td>
+
+                                            <td>
+                                              {job.profession.services[i].price}
+                                            </td>
+                                          </tr>
+                                        );
+                                      }
+                                    )
+                                  ) : (
+                                    <></>
+                                  )}
+                                </tbody>
+                              </table>
+                            </td>
+                            <td> {job.warranty} </td>
+                            <td> {job.payment_method} </td>
+                            {/*  <td> {job.description} </td> */}
+                            <td> {job.price} </td>
+                            <td>
+                              <Button variant="danger">Delete</Button>{" "}
+                            </td>
+                          </tr>
+                        );
+                      })
+                    ) : (
                       <tr>
-                        job={job} key={job._id}
-                        <td>{job.client}</td>
-                        <td>{job.employee}</td>
-                        <td>{job.profession}</td>
-                        <td>{job.service}</td>
-                        <td>{job.price}</td>
-                        <td>
-                          <button type="button" class="btn btn-danger">
-                            Delete
-                          </button>
-                        </td>
+                        <td colSpan="8">Wait for loading..</td>
                       </tr>
-                    ))}
+                    )}
                   </tbody>
                 </Table>
               </Card.Body>
