d6683c5927f8e23b39ea3b28af03fb385c8407f3
IslamAhmed0 <ea43533@gmail.com>
2021-06-16 00:30:36 +0200

    create profession by modal

---
+++ src/pages/dashboard/ServiceModal.js
@@ -0,0 +1,206 @@
+import { Button, Col, Container, Form, InputGroup,Modal,Row } from "react-bootstrap";
+import React, {useState} from 'react';
+import {render} from "@testing-library/react";
+import {Formik} from "formik";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
+import ModalDialog from 'react-bootstrap/ModalDialog'
+import * as yup from "yup";
+const schema = yup.object().shape({
+    title: yup.string().min(3).max(15).required("Title Required"),
+    description: yup.string().required("Description Required"),
+    service: yup.string().required("Service Required"),
+    img: yup.string().required("Img Required"),
+});
+function ServiceModal(props) {
+    const history = useHistory();
+
+    return (
+        <Modal {...props}
+               size="lg"
+               aria-labelledby="contained-modal-title-vcenter"
+               centered>
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    Using Grid in Modal
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body className="show-grid" >
+                <Container className="mt-3 w-100">
+                    <h2 className="mt-4 mb-4 text-center">Create Profession</h2>
+                    <Formik
+                        validationSchema={schema}
+                        onSubmit={async (values, actions) => {
+                            actions.setSubmitting(true);
+                            try {
+                                let serviceData = {};
+                                const formData = new FormData();
+                                for (let field in values) {
+                                    if (["service", "description", "price"].indexOf(field) !== -1) {
+                                        Object.assign(serviceData, { [field]: values[field] });
+                                    } else {
+                                        formData.append(field, values[field]);
+                                    }
+                                }
+                                formData.append("services", JSON.stringify([serviceData]));
+                                const added = await axios.post(
+                                    process.env.REACT_APP_API_URL + "/professions",
+                                    formData,
+                                    {
+                                        "Content-Type": "multipart/form-data",
+                                    }
+                                );
+                                if (added) {
+                                    console.log(added);
+                                    history.push("/admin/professions");
+                                }
+                                actions.setSubmitting(false);
+                            } catch (error) {
+                                console.error(error);
+                            }
+                        }}
+                        initialValues={{
+                            title: "",
+                            img: "",
+                            service: "",
+                            description: "",
+                            price: 0,
+                        }}
+                    >
+                        {({
+                              handleSubmit,
+                              handleChange,
+                              handleBlur,
+                              values,
+                              touched,
+                              errors,
+                          }) => (
+                            <Form
+                                noValidate
+                                encType="multipart/form-data"
+                                onSubmit={handleSubmit}
+                            >
+                                <Form.Row>
+                                    <Form.Group
+                                        as={Col}
+                                        md="10"
+
+
+                                        className="offset-1"
+                                        controlId="validationFormik101"
+                                    >
+                                        <Form.Label>Title</Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                            name="title"
+                                            value={values.title}
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            placeholder="Title"
+                                            isInvalid={touched.title && errors.title}
+                                        />
+                                        <Form.Control.Feedback type="invalid" tooltip>
+                                            {errors.title}
+                                        </Form.Control.Feedback>
+                                    </Form.Group>
+                                </Form.Row>
+                                <Form.Row>
+                                    <Form.Group
+                                        as={Col}
+                                        md="10"
+                                        className="offset-1"
+                                        controlId="validationFormik102"
+                                    >
+                                        <label>Image</label>
+                                        <Form.File
+                                            className="position-relative"
+                                            required
+                                            id="custom-file"
+                                            name="img"
+                                            label=""
+                                            placeholder="Image"
+                                            custom
+                                            onChange={(e) => (values.img = e.target.files[0])}
+                                            onBlur={handleBlur}
+                                            isInvalid={touched.img && errors.img}
+                                            feedback={errors.img}
+                                            feedbackTooltip
+                                        />
+                                    </Form.Group>
+                                </Form.Row>
+                                <Form.Row>
+                                    <Form.Group
+                                        as={Col}
+                                        md="5"
+                                        className="offset-1 "
+                                        controlId="validationFormik102"
+                                    >
+                                        <Form.Label>Service</Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                            placeholder="service"
+                                            name="service"
+                                            value={values.service}
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            isInvalid={touched.service && errors.service}
+                                        />
+                                        <Form.Control.Feedback type="invalid" tooltip>
+                                            {errors.service}
+                                        </Form.Control.Feedback>
+                                    </Form.Group>
+                                    <Form.Group as={Col} md="5" controlId="validationFormik104">
+                                        <Form.Label>Price</Form.Label>
+                                        <Form.Control
+                                            type="number"
+                                            placeholder="price"
+                                            name="price"
+                                            value={values.price}
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                        />
+                                    </Form.Group>
+                                </Form.Row>
+
+                                <Form.Row>
+                                    <Form.Group
+                                        as={Col}
+                                        md="10"
+                                        className="offset-1"
+                                        controlId="validationFormik103"
+                                    >
+                                        <Form.Label>Description</Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                            placeholder="description"
+                                            name="description"
+                                            value={values.description}
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            isInvalid={touched.description && errors.description}
+                                        />
+                                        <Form.Control.Feedback type="invalid" tooltip>
+                                            {errors.description}
+                                        </Form.Control.Feedback>
+                                    </Form.Group>
+                                </Form.Row>
+                                <Form.Row className="text-center">
+                                    <Button type="submit" style={{ margin: "auto" }}>
+                                        Save
+                                    </Button>
+                                </Form.Row>
+                            </Form>
+                        )}
+                    </Formik>
+                </Container>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button onClick={props.onHide}>Close</Button>
+            </Modal.Footer>
+        </Modal>
+    );
+}
+
+
+render(<ServiceModal />);
+export default ServiceModal
--- src/pages/dashboard/professions/index.js
+++ src/pages/dashboard/professions/index.js
@@ -2,12 +2,16 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import CollapseTable from "../../dashboard/Collapse";
 import { Link } from "react-router-dom";
-
+import ServiceModal from "../ServiceModal";
 // react-bootstrap components
 import { Card, Table, Container, Row, Col, Button } from "react-bootstrap";
 
 function Professions() {
-  const [professions, setProfessions] = useState([]);
+    const [professions, setProfessions] = useState([]);
+
+    const [modalShow, setModalShow] = useState(false);
+
+
   useEffect(() => {
     axios.get(process.env.REACT_APP_API_URL + "/professions").then((res) => {
       setProfessions(res.data);
@@ -16,7 +20,9 @@ function Professions() {
 
   return (
     <>
-      <Container fluid>
+        <ServiceModal show={modalShow} onHide={() => setModalShow(false)} />
+
+        <Container fluid>
         <Row>
           <Col md="12">
             <Card className="strpied-tabled-with-hover">
@@ -24,9 +30,10 @@ function Professions() {
                 <Card.Title as="h4">Professions</Card.Title>
                 <p className="card-category">control</p>
                 <div className="float-right">
-                  <button  to="/new"className="btn btn-primary">
-                    <i className="fa fa-plus"></i> <Link to="/admin/new">New Profession</Link>
-                  </button>
+                    <Button variant="primary" onClick={() => setModalShow(true)}>
+                        New Profession
+                    </Button>
+
                 </div>
               </Card.Header>
               <Card.Body className="table-full-width table-responsive px-0">
