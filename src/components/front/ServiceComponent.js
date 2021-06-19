d89762b93112599ee3e62197df39b901171369cb
Alshimaa safwat <shimaa.safwat112@gmail.com>
2021-06-20 01:57:20 +0200

    remove href from service component

--- src/components/front/ServiceComponent.js
+++ src/components/front/ServiceComponent.js
@@ -4,7 +4,7 @@ import "../../assets/front/css/profession.css";
 import "../../styles/Header.css";
 
 const ServiceComponent = ({ service }) => {
-  console.log("service comp",service)
+  console.log("service comp", service);
   return (
     <>
       <div className="col-lg-4 col-md-6 profession-wrapper">
@@ -14,11 +14,11 @@ const ServiceComponent = ({ service }) => {
             {/* <img src={`http://localhost:5000/uploads/professions/${profession.img}`} alt="profession" /> */}
           </div>
           <div className="services__item__text">
-            <a href="electricity_services.html">
-              <h4>{service.service}</h4>
-            </a>
+            <h4>{service.service}</h4>
             <p>{service.description}</p>
-            <p style={{color:"#111", fontWeight:"700"}}>price : {service.price}$</p>
+            <p style={{ color: "#111", fontWeight: "700" }}>
+              price : {service.price}$
+            </p>
           </div>
         </div>
       </div>
