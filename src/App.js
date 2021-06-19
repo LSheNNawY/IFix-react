4f7ca9994912d9c08815514ad34324fb0c615783
Alshimaa safwat <shimaa.safwat112@gmail.com>
2021-06-19 22:49:48 +0200

    fix conflict

--- src/App.js
+++ src/App.js
@@ -2,13 +2,10 @@ import { Route, Switch } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
-<<<<<<< HEAD
 import Profile from "./pages/Profile";
-=======
 import AboutUs from "./pages/AboutUs";
 // import Profile from "./pages/Profile";
 import Contact from "./pages/Contact";
->>>>>>> 0c73f0232ecf755aeaf2b94aa52b285e40d86eda
 
 import AdminLayout from "./layouts/Admin";
 
@@ -34,15 +31,9 @@ function App() {
                     path="/admin"
                     render={(props) => <AdminLayout {...props} />}
                 />
-<<<<<<< HEAD
                 <Route path="/profile/:id" render={(props) => <Profile {...props} />} />            
-=======
-
-
-
                 <Route path="/aboutUs" component={AboutUs} />
 
->>>>>>> 0c73f0232ecf755aeaf2b94aa52b285e40d86eda
             </Switch>
         </div>
     );
