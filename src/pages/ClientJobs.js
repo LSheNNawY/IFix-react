6b26071d3509a8992c5df4a4090481e4c4eaff2c
IslamAhmed0 <ea43533@gmail.com>
2021-06-26 19:50:13 +0200

    edit

--- src/pages/ClientJobs.js
+++ src/pages/ClientJobs.js
@@ -1,18 +1,16 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import "../assets/front/css/index.css";
 import dateFormat from "dateformat";
 import Review from "./Review";
-import Stripe from "../stripe/StripeContainer"
+import { ToastContainer } from "react-toastify";
+import Stripe from "../stripe/StripeContainer";
 
-const ClientJobs = ({job}) => {
+const ClientJobs = ({ job }) => {
     const [modalShow, setmodalShow] = useState(false);
-    const [modalstripe, setmodalstripe] = useState(false);
     const [empJob, setempJob] = useState(job);
+    const [successMsg, setSuccessMsg] = useState(false);
+    const [modalstripe, setmodalstripe] = useState(false);
 
-
-    useEffect(() => {
-    }, []);
-    console.log(empJob);
     return (
         <>
             <Review
@@ -20,15 +18,19 @@ const ClientJobs = ({job}) => {
                 setJob={setempJob}
                 show={modalShow}
                 onHide={() => setmodalShow(false)}
+                setSuccessMsg={setSuccessMsg}
             />
-            {
-                modalstripe ? <Stripe price={job.price} job_id={job._id} setJob={setempJob}
-                                      show={modalstripe}
-                                      onHide={() => setmodalstripe(false)}
-                /> : null
-            }
-
+            {modalstripe ? (
+                <Stripe
+                    price={job.price}
+                    job_id={job._id}
+                    setJob={setempJob}
+                    show={modalstripe}
+                    onHide={() => setmodalstripe(false)}
+                />
+            ) : null}
             <div className="container-fluid">
+                {successMsg ? <ToastContainer /> : null}
                 <div
                     className="row"
                     style={{
@@ -51,83 +53,97 @@ const ClientJobs = ({job}) => {
                                     <div className="col-xs-12 col-lg-8">
                                         <div className="row job_container">
                                             <div className="col-3">
-                                                <div className="services__item__icon  ">
+                                                <div className="services_item_icon  ">
                                                     <i className="fas fa-bolt"></i>
                                                 </div>
                                             </div>
                                             <div className="col-9">
-                                                <div className="services__item__text"></div>
+                                                <div className="services_item_text"></div>
                                                 <div className="dates">
                                                     <div className="row">
                                                         <div className="col-6 ">
                                                             <h4>
                                                                 Started At:{" "}
-                                                                <h6 className="badge badge-success">
-                                                                    {empJob.started_at
-                                                                        ? dateFormat(
-                                                                            job.started_at,
+                                                                {empJob.started_at ? (
+                                                                    <i className="badge badge-success h6">
+                                                                        {dateFormat(
+                                                                            empJob.started_at,
                                                                             "mmmm dS, yyyy - h:MM TT"
-                                                                        )
-                                                                        : ""}
-                                                                </h6>
+                                                                        )}
+                                                                    </i>
+                                                                ) : (
+                                                                    <div>
+                                                                        <i className="badge badge-warning h6">
+                                                                            Pending
+                                                                        </i>
+                                                                    </div>
+                                                                )}
                                                             </h4>
                                                         </div>
                                                         <div className="col-6 ">
                                                             <h4>
                                                                 Ended At:{" "}
-                                                                <h6 className="badge badge-success">
-                                                                    {empJob.ended_at
-                                                                        ? dateFormat(
-                                                                            job.ended_at,
+                                                                {empJob.ended_at ? (
+                                                                    <i className="badge badge-success h6">
+                                                                        {dateFormat(
+                                                                            empJob.started_at,
                                                                             "mmmm dS, yyyy - h:MM TT"
-                                                                        )
-                                                                        : ""}
-                                                                </h6>
+                                                                        )}
+                                                                    </i>
+                                                                ) : (
+                                                                    <div>
+                                                                        <i className="badge badge-warning h6">
+                                                                            Pending
+                                                                        </i>
+                                                                    </div>
+                                                                )}
                                                             </h4>
                                                         </div>
                                                     </div>
 
                                                     <div className="row ">
                                                         <div className="col-6 ">
-                                                            <h4>
-                                                                Price :{" "}
-                                                                <h6 className="badge badge-success">
-                                                                    {empJob.price} EGP
-                                                                </h6>{" "}
-                                                            </h4>
+                                                            <span className="h4 d-block my-1">Price :</span>
+                                                            <i
+                                                                className={`${
+                                                                    empJob.ended_at
+                                                                        ? "h6 badge badge-success"
+                                                                        : "h6"
+                                                                }`}
+                                                            >
+                                                                {empJob.price} EGP
+                                                            </i>
                                                         </div>
                                                         {empJob.review === undefined &&
                                                         empJob.ended_at !== undefined ? (
                                                             <div
-                                                                className="col-3"
+                                                                className="col-2"
                                                                 onClick={() => setmodalShow(true)}
-                                                                style={{cursor: "pointer"}}
+                                                                style={{ cursor: "pointer" }}
                                                             >
                                                                 <i
                                                                     className="fas fa-comment"
-                                                                    style={{fontSize: "30px", padding: "30px"}}
+                                                                    style={{ fontSize: "30px", padding: "30px" }}
                                                                 ></i>
                                                             </div>
                                                         ) : (
                                                             ""
                                                         )}
-                                                        {
-
-                                                            empJob.ended_at === undefined ? (
-                                                                <div
-                                                                    className="col-3"
-                                                                    onClick={() => setmodalstripe(true)}
-                                                                    style={{cursor: "pointer"}}
-                                                                >
-                                                                    <i
-                                                                        className="fas fa-credit-card"
-                                                                        style={{fontSize: "30px", padding: "30px"}}
-                                                                        aria-hidden="true"
-                                                                    ></i>
-                                                                </div>
-                                                            ) : (
-                                                                ""
-                                                            )}
+                                                        {empJob.ended_at === undefined ? (
+                                                            <div
+                                                                className="col-3"
+                                                                onClick={() => setmodalstripe(true)}
+                                                                style={{ cursor: "pointer" }}
+                                                            >
+                                                                <i
+                                                                    className="fas fa-credit-card"
+                                                                    style={{ fontSize: "30px", padding: "30px" }}
+                                                                    aria-hidden="true"
+                                                                ></i>
+                                                            </div>
+                                                        ) : (
+                                                            ""
+                                                        )}
                                                     </div>
                                                 </div>
                                             </div>
