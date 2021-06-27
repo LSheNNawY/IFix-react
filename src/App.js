c8ced2212f33b390a42fdde8f48dd204b3fdedcc
sara-mohamed12 <78587095+sara-mohamed12@users.noreply.github.com>
2021-06-27 11:38:12 +0200

    handel login and register route

--- src/App.js
+++ src/App.js
@@ -20,18 +20,40 @@ import UserContext from "./context/UserContext";
 import axios from "axios";
 
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import { render } from "@testing-library/react";
 
 axios.defaults.withCredentials = true;
 
 function App() {
   const { user } = useContext(UserContext);
+  //console.log(user);
   return (
     <div className="App">
       <Switch>
         <Route path="/" component={Home} exact />
-        <Route path="/login" component={Login} />
+        <Route
+          exact
+          path="/login"
+          render={() => {
+            if (user){
+              //alert("You can't login if you are logged in!")
+              return <Home />;
+            }
+            else return <Login />;
+          }}
+        />
         <Route path="/logout" component={Logout} />
-        <Route path="/register" component={Register} />
+        <Route
+          exact
+          path="/register"
+          render={() => {
+            if (user){
+              alert("You are already registered!")
+              return <Home />;
+            }
+            else return <Register />;
+          }}
+        />
         <Route path="/contact" component={Contact} />
         <Route path="/aboutUs" component={AboutUs} />
         <Route path="/order" component={Order} />
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,193 +6,177 @@ import { authFormValidation } from "../helpers/loginValidation";
 import UserContext from "../context/UserContext";
 
 import "../assets/front/css/login.css";
+import { date } from "yup/lib/locale";
 const ajaxLogin = async (email, password) => {
-    const data = await (
-        await fetch(`${process.env.REACT_APP_API_URL}/users/login`, {
-            method: "POST",
-            credentials: "include",
-            headers: {
-                "Content-Type": "Application/json",
-            },
-            body: JSON.stringify({ email, password }),
-        })
-    ).json();
-    return data;
+  const data = await (
+    await fetch(`${process.env.REACT_APP_API_URL}/users/login`, {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "Application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    })
+  ).json();
+  console.log(data)
+  return data;
 };
 
-const Login = () => {
+const Login = (props) => {
     const [user, setUser] = useState({ email: "", password: "" });
     const [errors, setErrors] = useState({ email: "", password: "" });
     const [loggingError, setLoggingError] = useState("");
     const { getUser } = useContext(UserContext);
+    console.log(setUser);
+  //console.log(user);
 
-    const history = useHistory();
-    const submitHandler = async (e) => {
-        e.preventDefault();
-        if (authFormValidation(user.email, user.password, setErrors)) {
-            try {
-                const userData = await ajaxLogin(user.email, user.password);
-                console.log("user data = ", userData);
-                if (userData.userId) {
-                    localStorage.setItem(
-                        "user",
-                        JSON.stringify({
-                            userId: userData.userId,
-                            email: userData.email,
-                        })
-                    );
-                    await getUser();
-                    history.push("/");
-                } else {
-                    setLoggingError("Wrong user!");
-                }
-            } catch (err) {
-                console.log(err);
-            }
+  const history = useHistory();
+  const { id } = props
+  console.log("id= ",id)
+  
+
+   
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    if (authFormValidation(user.email, user.password, setErrors)) {
+      try {
+        const userData = await ajaxLogin(user.email, user.password);
+        console.log("user data = ", userData);
+        if (userData.userId) {
+          localStorage.setItem(
+            "user",
+            JSON.stringify({
+              userId: userData.userId,
+              email: userData.email,
+            })
+          );
+          await getUser();
+
+          history.push("/");
+        } else {
+          setLoggingError("Please Register First");
         }
-    };
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
 
-    return (
-        <div className="login-wrapper">
-            <NavbarComponent />
-            <div
-                style={{
-                    backgroundColor: "#ebeeef",
-                    paddingTop: "120px",
-                    paddingBottom: "120px",
-                    marginTop: "-80px",
-                }}
-            >
-                <div className="container ">
-                    <div className="row">
-                        <div className=" login-form col-lg-6 col-md-8">
-                            <div className="login-form-title">
-                                <span className="login-form-title-1">
-                                    login
-                                </span>
-                            </div>
+  return (
+    <div className="login-wrapper">
+      <NavbarComponent />
+      <div
+        style={{
+          backgroundColor: "#ebeeef",
+          paddingTop: "120px",
+          paddingBottom: "120px",
+          marginTop: "-80px",
+        }}
+      >
+        <div className="container ">
+          <div className="row">
+            <div className=" login-form col-lg-6 col-md-8">
+              <div className="login-form-title">
+                <span className="login-form-title-1">login</span>
+              </div>
 
-                            <form onSubmit={submitHandler}>
-                                <div className="mb-3">
-                                    <label
-                                        htmlFor="exampleInputEmail1"
-                                        className="form-label"
-                                    >
-                                        Email address
-                                    </label>
-                                    <div className="input-group-prepend">
-                                        <span
-                                            className={`input-group ${
-                                                errors.email !== "" &&
-                                                errors.email !== "valid"
-                                                    ? "border-danger"
-                                                    : ""
-                                            }`}
-                                        ></span>
-                                    </div>
-                                    <input
-                                        type="email"
-                                        className={`form-control  ${
-                                            errors.email !== "" &&
-                                            errors.email !== "valid"
-                                                ? "is-invalid"
-                                                : ""
-                                        }`}
-                                        id="exampleInputEmail1"
-                                        aria-describedby="emailHelp"
-                                        name="email"
-                                        onChange={(e) =>
-                                            setUser({
-                                                ...user,
-                                                email: e.target.value,
-                                            })
-                                        }
-                                        value={user.email}
-                                        placeholder="Enter your email"
-                                    />
-                                    {errors.email !== "" &&
-                                    errors.email !== "valid" ? (
-                                        <h6 className="invalid-feedback">
-                                            {errors.email}
-                                        </h6>
-                                    ) : null}
-                                </div>
-                                <div className="mb-3">
-                                    <label
-                                        htmlFor="exampleInputPassword1"
-                                        className="form-label"
-                                    >
-                                        Password
-                                    </label>
-                                    <div className="input-group-prepend">
-                                        <span
-                                            className={`input-group${
-                                                errors.password !== "" &&
-                                                errors.password !== "valid"
-                                                    ? "border-danger"
-                                                    : ""
-                                            }`}
-                                        ></span>
-                                    </div>
-                                    <input
-                                        type="password"
-                                        className={`form-control  ${
-                                            errors.password !== "" &&
-                                            errors.password !== "valid"
-                                                ? "is-invalid"
-                                                : ""
-                                        }`}
-                                        id="exampleInputPassword1"
-                                        name="password"
-                                        onChange={(e) =>
-                                            setUser({
-                                                ...user,
-                                                password: e.target.value,
-                                            })
-                                        }
-                                        value={user.password}
-                                        placeholder="Enter your password"
-                                    />
-                                    {errors.password !== "" &&
-                                    errors.password !== "valid" ? (
-                                        <h6 className="invalid-feedback">
-                                            {errors.password}
-                                        </h6>
-                                    ) : null}
-                                </div>
-                                {loggingError ? (
-                                    <h6 className="text-danger pb-3">
-                                        {loggingError}
-                                    </h6>
-                                ) : (
-                                    ""
-                                )}
-
-                                <button
-                                    type="submit"
-                                    className="btn btn-primary mb-3 site-btn"
-                                >
-                                    Submit
-                                </button>
-                                <div className="text-center w-100">
-                                    <p className="text-muted font-weight-bold">
-                                        Not a member?
-                                        <Link
-                                            to="/register"
-                                            className=" ml-2 text-warning"
-                                        >
-                                            Register
-                                        </Link>
-                                    </p>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
+              <form onSubmit={submitHandler}>
+                <div className="mb-3">
+                  <label htmlFor="exampleInputEmail1" className="form-label">
+                    Email address
+                  </label>
+                  <div className="input-group-prepend">
+                    <span
+                      className={`input-group ${
+                        errors.email !== "" && errors.email !== "valid"
+                          ? "border-danger"
+                          : ""
+                      }`}
+                    ></span>
+                  </div>
+                  <input
+                    type="email"
+                    className={`form-control  ${
+                      errors.email !== "" && errors.email !== "valid"
+                        ? "is-invalid"
+                        : ""
+                    }`}
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                    name="email"
+                    onChange={(e) =>
+                      setUser({
+                        ...user,
+                        email: e.target.value,
+                      })
+                    }
+                    value={user.email}
+                    placeholder="Enter your email"
+                  />
+                  {errors.email !== "" && errors.email !== "valid" ? (
+                    <h6 className="invalid-feedback">{errors.email}</h6>
+                  ) : null}
                 </div>
+                <div className="mb-3">
+                  <label htmlFor="exampleInputPassword1" className="form-label">
+                    Password
+                  </label>
+                  <div className="input-group-prepend">
+                    <span
+                      className={`input-group${
+                        errors.password !== "" && errors.password !== "valid"
+                          ? "border-danger"
+                          : ""
+                      }`}
+                    ></span>
+                  </div>
+                  <input
+                    type="password"
+                    className={`form-control  ${
+                      errors.password !== "" && errors.password !== "valid"
+                        ? "is-invalid"
+                        : ""
+                    }`}
+                    id="exampleInputPassword1"
+                    name="password"
+                    onChange={(e) =>
+                      setUser({
+                        ...user,
+                        password: e.target.value,
+                      })
+                    }
+                    value={user.password}
+                    placeholder="Enter your password"
+                  />
+                  {errors.password !== "" && errors.password !== "valid" ? (
+                    <h6 className="invalid-feedback">{errors.password}</h6>
+                  ) : null}
+                </div>
+                {loggingError ? (
+                  <h6 className="text-danger pb-3">{loggingError}</h6>
+                ) : (
+                  ""
+                )}
+
+                <button type="submit" className="btn btn-primary mb-3 site-btn">
+                  Submit
+                </button>
+                <div className="text-center w-100">
+                  <p className="text-muted font-weight-bold">
+                    Not a member?
+                    <Link to="/register" className=" ml-2 text-warning">
+                      Register
+                    </Link>
+                  </p>
+                </div>
+              </form>
             </div>
-            <FooterComponent />
+          </div>
         </div>
-    );
+      </div>
+      <FooterComponent />
+    </div>
+  );
 };
 
 export default Login;
