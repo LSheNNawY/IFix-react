0090b6b0558506b1caaf3366f2a96b4cdcdf507f
LSheNNawY <devshennawy@gmail.com>
2021-07-03 22:34:39 +0200

    fix home user picture

--- src/components/front/EmployeeComponent.js
+++ src/components/front/EmployeeComponent.js
@@ -3,22 +3,30 @@ import { Link } from "react-router-dom";
 import empImg from "../../assets/front/img/employees/employee1.jpg";
 
 export default function Employee({ employee }) {
-  console.log(employee);
+  console.log(employee.image);
   const ratingArr = new Array(Math.ceil(employee.rating) || 3).fill(null);
 
-  console.log(ratingArr);
   return (
     <>
       <div className="col-lg-3 col-md-6" key={employee._id}>
         <Link to={`/profile/employee/${employee._id}`}>
-          <img src={employee.image ?? empImg} alt="img" />
+          <img
+            src={
+              employee.picture
+                ? `${process.env.REACT_APP_API_PUBLIC_URL}/uploads/users/${employee.picture}`
+                : empImg
+            }
+            alt="img"
+          />
         </Link>
         <div className="team__item__text">
           <Link to={`/profile/employee/${employee._id}`}>
             <h4>{`${employee.firstName} ${employee.lastName}`}</h4>
           </Link>
           <div className="rate">
-            {ratingArr.map((v, i) => <i className="fas fa-star" key={i}></i>)}
+            {ratingArr.map((v, i) => (
+              <i className="fas fa-star" key={i}></i>
+            ))}
           </div>
         </div>
       </div>
